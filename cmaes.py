adf296ccaf598a00902c7ec0cb4e0456140d531a
Artimi <petrsebek1@gmail.com>
2013-04-16 23:42:26 +0200

    Cmaes class
    Made CMAES class for better manipulation with method than just by
    function. Replaced old file cmaes.py

--- cmaes.py
+++ cmaes.py
@@ -1,99 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from __future__ import division
 import numpy as np
-# import pprint as pp
-# import ipdb
 
 
-def cmaes(strfitnessfct, N):
-    # input parameters
-    xmean = np.random.rand(N)
-    sigma = 0.5
-    stopfitness = 1e-30
-    stopeval = 1e4 * N ** 2
+class CMAES(object):
 
-    # strategy parameter setting: selection
-    lamda = int(4 + 3 * np.log(N))
-    mu = lamda / 2
-    weights = np.array([np.log(mu + 0.5) - np.log(i) for i in range(1, int(mu) + 1)])
-    mu = int(mu)
-    weights = weights / np.sum(weights)
-    mueff = 1 / np.sum(weights ** 2)
+    def __init__(self, N, sigma=0.3, xmean=None):
+        self.N = N
+        if xmean is None:
+            self.xmean = np.random.rand(N)
+        else:
+            self.xmean = xmean
+        self.end = False
+        self.sigma = sigma
+        self.stopfitness = 1e-10
+        self.stopeval = 1e3 * N ** 2
+        # strategy parameter setting: selection
+        self.lamda = 2 * int(4 + 3 * np.log(self.N))
+        self.mu = self.lamda / 2
+        self.weights = np.array([np.log(self.mu + 0.5) - np.log(i)
+                                 for i in range(1, int(self.mu) + 1)])
+        self.mu = int(self.mu)
+        self.weights = self.weights / np.sum(self.weights)
+        self.mueff = 1 / np.sum(self.weights ** 2)
+        # strategy parameter setting: adaptation
+        self.cc = (4 + self.mueff / N) / (N + 4 + 2 * self.mueff / N)
+        self.cs = (self.mueff + 2) / (N + self.mueff + 5)
+        self.c1 = 2 / ((self.N + 1.3) ** 2 + self.mueff)
+        self.cmu = min(1 - self.c1, 2 * (self.mueff - 2 + 1 / self.mueff) /
+                       ((self.N + 2) ** 2 + self.mueff))
+        self.damps = 1 + 2 * max(0, np.sqrt((self.mueff - 1) /
+                                (self.N + 1)) - 1) + self.cs
+        # initialize dynamic (internal) strategy parameters and constants
+        self.pc = np.zeros((1, self.N))
+        self.ps = np.zeros_like(self.pc)
+        self.B = np.eye(self.N)
+        self.D = np.eye(self.N)
+        self.C = np.identity(self.N)
+        self.eigenval = 0
+        self.chiN = self.N ** 0.5 * (1 - 1 / (4 * self.N) + 1 /
+                                     (21 * self.N ** 2))
+        # generation loop
+        self.counteval = 0
+        self.iteration = 0
+        #self.arx = np.empty([self.N, self.lamda])
+        #self.arz = np.empty_like(self.arx)
+        #self.arfitness = np.empty(self.lamda)
 
-    # strategy parameter setting: adaptation
-    cc = (4 + mueff / N) / (N + 4 + 2 * mueff / N)
-    cs = (mueff + 2) / (N + mueff + 5)
-    c1 = 2 / ((N + 1.3) ** 2 + mueff)
-    cmu = min(1 - c1, 2 * (mueff - 2 + 1 / mueff) / ((N + 2) ** 2 + mueff))
-    damps = 1 + 2 * max(0, np.sqrt((mueff - 1) / (N + 1)) - 1) + cs
+    def ask(self, lamda=None, xmean=None):
+        if lamda is not None:
+            self.lamda = lamda
+        self.arz = np.random.randn(self.lamda, self.N)
+        self.arx = self.xmean + self.sigma * np.dot(np.dot(self.B, self.D), self.arz.T).T
+        return self.arx
 
-    # initialize dynamic (internal) strategy parameters and constants
-    pc = np.zeros((1, N))
-    ps = np.zeros((1, N))
-    B = np.eye(N)
-    D = np.eye(N)
-    C = np.identity(N)
-    eigenval = 0
-    chiN = N ** 0.5 * (1 - 1 / (4 * N) + 1 / (21 * N ** 2))
 
-    # generation loop
-    counteval = 0
-    arx = np.empty([N, lamda])
-    arz = np.empty([N, lamda])
-    arfitness = np.empty(lamda)
-    while counteval < stopeval:
-        for k in range(lamda):
-            arz[:, k] = np.random.randn(N)
-            # arx[:, k] = np.random.multivariate_normal(xmean, sigma ** 2 * C)
-            arx[:, k] = xmean + sigma * (np.dot(np.dot(B, D), arz[:, k]))
-            arfitness[k] = strfitnessfct(arx[:, k])
-            counteval += 1
+    def tell(self, arfitness):
+        self.arfitness = arfitness
+        self.iteration += 1
+        self.counteval += self.lamda
+        self.arindex = np.argsort(self.arfitness)
         # sort by fitness and compute weighted mean into xmean
-        arindex = np.argsort(arfitness)
-        arfitness = arfitness[arindex]
-        # xold = xmean
-        xmean = np.dot(arx[:, arindex[:mu]], weights)
-        zmean = np.dot(arz[:, arindex[:mu]], weights)
-
-        ps = np.dot((1 - cs), ps) + np.dot((np.sqrt(cs * (2 - cs) * mueff)), np.dot(B, zmean))
-        hsig = np.linalg.norm(ps) / np.sqrt(1 - (1 - cs) ** (2 * counteval / lamda)) / chiN < 1.4 + 2 / (N + 1)
-        pc = np.dot((1 - cc), pc) + np.dot(np.dot(hsig, np.sqrt(cc * (2 - cc) * mueff)), np.dot(np.dot(B, D), zmean))
+        self.arindex = np.argsort(self.arfitness)
+        self.arfitness = self.arfitness[self.arindex]
+        self.xmean = np.dot(self.arx[self.arindex[:self.mu]].T, self.weights)
+        self.zmean = np.dot(self.arz[self.arindex[:self.mu]].T, self.weights)
+        self.ps = np.dot((1 - self.cs), self.ps) + np.dot((np.sqrt(self.cs * (2 - self.cs) * self.mueff)), np.dot(self.B, self.zmean))
+        self.hsig = np.linalg.norm(self.ps) / np.sqrt(1 - (1 - self.cs) ** (2 * self.counteval / self.lamda)) / self.chiN < 1.4 + 2 / (self.N + 1)
+        self.pc = np.dot((1 - self.cc), self.pc) + np.dot(np.dot(self.hsig, np.sqrt(self.cc * (2 - self.cc) * self.mueff)), np.dot(np.dot(self.B, self.D), self.zmean))
         # adapt covariance matrix C
-        C = np.dot((1 - c1 - cmu), C) \
-            + np.dot(c1, ((pc * pc.T)
-            + np.dot((1 - hsig) * cc * (2 - cc), C))) \
-            + np.dot(cmu,
-                     np.dot(np.dot(np.dot(np.dot(B, D), arz[:, arindex[:mu]]),
-                            np.diag(weights)), (np.dot(np.dot(B, D), arz[:, arindex[:mu]])).T))
-
+        self.C = np.dot((1 - self.c1 - self.cmu), self.C) \
+            + np.dot(self.c1, ((self.pc * self.pc.T)
+            + np.dot((1 - self.hsig) * self.cc * (2 - self.cc), self.C))) \
+            + np.dot(self.cmu,
+                     np.dot(np.dot(np.dot(np.dot(self.B, self.D), self.arz[self.arindex[:self.mu]].T),
+                            np.diag(self.weights)), (np.dot(np.dot(self.B, self.D), self.arz[self.arindex[:self.mu]].T)).T))
         # adapt step size sigma
-        sigma = sigma * np.exp((cs / damps) * (np.linalg.norm(ps) / chiN - 1))
-
+        self.sigma = self.sigma * np.exp((self.cs / self.damps) * (np.linalg.norm(self.ps) / self.chiN - 1))
         # diagonalization
-        if counteval - eigenval > lamda / (c1 + cmu) / N / 10:
-            eigenval = counteval
-            C = np.triu(C) + np.triu(C, 1).T
-            #ipdb.set_trace()
-            D, B = np.linalg.eig(C)
-            D = np.diag(np.sqrt(D))
-            # invsqrtC = np.dot(np.dot(B, np.diag(D ** -1)), B.T)
+        if self.counteval - self.eigenval > self.lamda / (self.c1 + self.cmu) / self.N / 10:
+            self.eigenval = self.counteval
+            self.C = np.triu(self.C) + np.triu(self.C, 1).T
+            self.D, self.B = np.linalg.eig(self.C)
+            self.D = np.diag(np.sqrt(self.D))
+        if self.arfitness[0] <= self.stopfitness:
+            self.end = True
+            self.result = self.arx[self.arindex[0]]
+        
 
-        if arfitness[0] <= stopfitness:
-            break
+def fmin(func, N):
+    cma = CMAES(N)
+    while not cma.end:
+        pop = cma.ask()
+        values = np.empty(pop.shape[0])
+        for i in xrange(pop.shape[0]):
+            values[i] = func(pop[i])
+        cma.tell(values)
+    print "Result: {0}\nFunction value: {1}".format(cma.result, cma.arfitness[0])
 
-    return arx[:, arindex[0]], counteval
-
-
-def f2(x):
-    """
-    Rosenbrock banana function
-    minimum: (1, 1)
-    f2((1,1)) == 0.0
-    """
-    return 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2
-
-
-if __name__ == '__main__':
-    res, evals = cmaes(f2, 2)
-    print "result:", res
-    print "evals:", evals
-    print "function value:", f2(res)
